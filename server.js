db6b4deeba3f64a98e2d94179982a06886d16cda
Nikki K <808nikz@gmail.com>
2016-07-30 19:08:23 -1000

    passport-local-auth

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var db = require('./models');
 var CONFIG = require('./config');
+var DB_CONFIG = require('./config/config');
 
 var pug = require('pug');
 var express = require('express');
@@ -9,13 +10,18 @@ var path = require('path'); // absolute path.
 var util = require('util');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 // var querystring = require('querystring');
-var passport = require('passport');
-var BasicStrategy = require('passport-http').BasicStrategy;
+// var passport = require('passport');
+// var BasicStrategy = require('passport-http').BasicStrategy;
+
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
 
 var Gallery = require('./Gallery');
 var Form = require('./Form');
+
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(methodOverride(function(req, res){
@@ -27,6 +33,63 @@ app.use(methodOverride(function(req, res){
   }
 }));
 
+passport.serializeUser(function(user, done) {
+  // user is passed in from local strategy.
+  // user is attached to req.user.
+  done(null, user); // this gets saved into the session store
+});
+
+passport.deserializeUser(function(userId, done) {
+  // deserializeUser finds the user.
+  var user = null;
+  // Find the user before you invoke done.
+  if (userId === 1){
+    user = {
+      
+    }
+  }
+  // This becomes your req.user.
+  done(null, user);
+});
+
+function isAuthenticated (req, res, next) {
+  if(!req.isAuthenticated()){
+    return res.redirect('/login');
+  }
+  return next();
+}
+
+app.get('/secret', function (req, res) {
+
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  var USERNAME = DB_CONFIG.SECRETS.USERNAME;
+  var PASSWORD = DB_CONFIG.SECRETS.PASSWORD;
+  var isAuthenticated = authenticate(username, password);
+   if (username === USERNAME && password === PASSWORD) {
+    return done(null, {});
+   }
+   Users.findOne({
+    username = username,
+    password = password
+    // if successful or Found
+    // return done
+    /// else
+    // redirect or return false
+   }).then(function(response) {
+
+   });
+  }
+));
+
+app.use(passport.initialize());
+app.use(passport.session({
+  secret: CONFIG.SESSION.secret,
+  saveUninitialized: false,
+  resave: true
+}));
 
 // allows you to save value to your configuration.
 app.set('views', path.resolve(__dirname, 'views'));
@@ -34,15 +97,15 @@ app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
-var user = { username: 'bob', password: 'secret', email: 'bob@example.com' };
-passport.use(new BasicStrategy(
-  function(username, password, done) {
-    // Example authentication strategy using 
-    if ( !(username === user.username && password === user.password) ) {
-      return done(null, false);
-    }
-    return done(null, user);
-}));
+// var user = { username: 'bob', password: 'secret', email: 'bob@example.com' };
+// passport.use(new BasicStrategy(
+//   function(username, password, done) {
+//     // Example authentication strategy using 
+//     if ( !(username === user.username && password === user.password) ) {
+//       return done(null, false);
+//     }
+//     return done(null, user);
+// }));
 
 app.get('/', function (req, res) {
   // get is the rendering of the page.
@@ -60,8 +123,21 @@ app.get('/', function (req, res) {
   });
 });
 
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+
+app.get('/secret', function (req, res) {
+  res.render('secret');
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/secret',
+  failureRedirect: '/login'
+}));
+
 app.get('/gallery/new',
-  passport.authenticate('basic', { session: false }),
+  // passport.authenticate('basic', { session: false }),
   function(req, res) {
     // res.json(req.user);
     res.render('form');
